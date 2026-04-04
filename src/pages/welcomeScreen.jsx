78f3f03e0cb82c739e9cf2b7d6a5ba96088aa7e3
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2026-04-04 17:03:52 -0400

    Changed Welcome Screen text

--- src/pages/welcomeScreen.jsx
+++ src/pages/welcomeScreen.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../global.css'
 
 
 function WelcomeScreen() {
     return (
         <div className="welcome-screen">
-            <h1>Welcome to the React App</h1>
-            <p>This is a simple React app with a welcome screen.</p>
+            <h1>Hi, my name is Sankalp Amaravadi</h1>
+            <p>A senior at Kennesaw State University working towards a Bachelor's in Software Engineering with a minor in Computer Science.</p>
+
+            <ul>
+                <li>I recently finished a JS based project with a nonprofit.</li>
+                <li>My current project is a web based accountiong application</li>
+                <li>I'm currently developing my Python, JavaScript, and C++ skillset.</li>
+                <li>My side projects will be posted on this website at a later time</li>
+                <li>Fun fact: I am a car enthusiast, like photography, and I play disc golf for fun!</li>
+            </ul>
         </div>
     );
 }
