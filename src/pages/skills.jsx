ee5b47f97a47a4693298412c9a474d9d1538f953
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2026-04-11 18:05:51 -0400

    Added skills and visual logic

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -5,7 +5,88 @@ function Skills() {
     return (
         <div>
             <h1>Skills</h1>
-            <p>This is the Skills page.</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Programming language</th>
+                        <th>Years of Experience</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Java</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>JavaScript</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>Python</td>
+                        <td>1 year</td>
+                    </tr>
+                    <tr>
+                        <td>React.JS</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>Node.JS</td>
+                        <td>1 year</td>
+                    </tr>
+                    <tr>
+                        <td>HTML</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>CSS</td>
+                        <td>1 year</td>
+                    </tr>
+                    <tr>
+                        <td>SQL</td>
+                        <td>1 year</td>
+                    </tr>
+                    <tr>
+                        <td>Docker</td>
+                        <td>8 months</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br />
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tools</th>
+                        <th>Years of Experience</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>GitHub & Git</td>
+                        <td>4 years</td>
+                    </tr>
+                    <tr>
+                        <td>JetBrains IDE Suite</td>
+                        <td>3 years</td>
+                    </tr>
+                    <tr>
+                        <td>MS Office Suite</td>
+                        <td>10 years</td>
+                    </tr>
+                    <tr>
+                        <td>CI/CD Pipelines</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>Bash</td>
+                        <td>2 years</td>
+                    </tr>
+                    <tr>
+                        <td>Trello</td>
+                        <td>1 year</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
