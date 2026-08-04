ecb8d91e0ba66423fb9bce7ee0510341d6d46d26
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2026-08-04 16:26:14 -0400

    Fixed experience page test

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -5,9 +5,8 @@ function Experience() {
     return (
         <main className="page">
             <h1>Experience</h1>
-            <p>ummarize roles, impact, and technologies here. Consider mirroring your résumé with a bit
+            <p>Summarize roles, impact, and technologies here. Consider mirroring your résumé with a bit
                 more room for outcomes and lessons learned.
-                S
             </p>
         </main>
     );
